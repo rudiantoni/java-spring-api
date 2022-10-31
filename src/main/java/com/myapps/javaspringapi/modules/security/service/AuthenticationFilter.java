31e99c2e3c54df2679cc4573221c2c28f77bbc61
Rudi Antoni <rudiantoni2013@gmail.com>
2022-10-31 13:28:36 -0300

    Implementing security

--- src/main/java/com/myapps/javaspringapi/modules/security/service/AuthenticationFilter.java
+++ src/main/java/com/myapps/javaspringapi/modules/security/service/AuthenticationFilter.java
@@ -1,8 +1,9 @@
 package com.myapps.javaspringapi.modules.security.service;
+
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
+
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
